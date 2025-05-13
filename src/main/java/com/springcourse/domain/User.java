8775208fc24e996e32eaca4f643c239016fae808
Pchiappini <patricia_chiappini@yahoo.com.br>
2025-05-13 10:47:07 -0300

    Add Role attribute

--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -3,6 +3,8 @@ package com.springcourse.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.springcourse.enums.Role;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +18,10 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
+	
 	
 
 	
