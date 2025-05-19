2b347b714d752825546c956dc02b7fc2ecf02171
rndsilva <rafaelnunesatrix@gmail.com>
2025-05-19 01:31:16 -0300

    Create request stage repository tests

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -22,9 +22,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter @Setter
 @Entity(name = "request_stage")
 public class RequestStage implements Serializable {
 	
@@ -40,7 +37,7 @@ public class RequestStage implements Serializable {
 	@Column(columnDefinition = "text")
 	private String description;
 	
-	@Column(name ="realization_date", nullable = false)
+	@Column(name ="realization_date", nullable = false, updatable = false)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizationDate;
 	
@@ -55,5 +52,72 @@ public class RequestStage implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "owner_id", nullable = false)
 	private User owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getRealizationDate() {
+		return realizationDate;
+	}
+
+	public void setRealizationDate(Date realizationDate) {
+		this.realizationDate = realizationDate;
+	}
+
+	public RequestState getState() {
+		return state;
+	}
+
+	public void setState(RequestState state) {
+		this.state = state;
+	}
+
+	public Request getRequest() {
+		return request;
+	}
+
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public RequestStage(Long id, String description, Date realizationDate, RequestState state, Request request,
+			User owner) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.realizationDate = realizationDate;
+		this.state = state;
+		this.request = request;
+		this.owner = owner;
+	}
+	
+	public RequestStage() {
+		
+	}
 
 }
---
+++ src/test/java/com/springcourse/repository/RequestStageRepositoryTests.java
@@ -0,0 +1,63 @@
+package com.springcourse.repository;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.enums.RequestState;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class RequestStageRepositoryTests {
+
+	@Autowired private RequestStageRepository requestStageRepository;
+	
+	@Test
+	@Order(1)
+	public void saveTest() {
+		
+		User owner = new User();
+		owner.setId(1L);
+		
+		Request request = new Request();
+		request.setId(1L);
+		RequestStage stage = new RequestStage(null, "Foi comprado um novo laptor de marca HP e com 16GB de RAM", new Date(), RequestState.CLOSED, request, owner);
+		
+		RequestStage createdStage = requestStageRepository.save(stage);
+		
+		assertThat(createdStage.getId()).isEqualTo(1L);
+		
+				
+	}
+	
+	@Test
+	@Order(2)
+	public void getByIdTest() {
+		Optional<RequestStage> result = requestStageRepository.findById(1L);
+		RequestStage stage = result.get();
+		
+		assertThat(stage.getDescription()).isEqualTo("Foi comprado um novo laptor de marca HP e com 16GB de RAM");
+		
+	}
+	
+	@Test
+	@Order(3)
+	public void listByRequestIdTet() {
+		List<RequestStage> stages = requestStageRepository.findAll();
+		
+		assertThat(stages.size()).isEqualTo(1);
+		
+	}
+}
