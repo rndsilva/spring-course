6d3f55cd8060bf230cefd44e4e179a1935ac522c
rndsilva <rafaelnunesatrix@gmail.com>
2025-05-21 17:48:13 -0300

    create user resource

--- src/main/java/com/springcourse/service/RequestStageService.java
+++ src/main/java/com/springcourse/service/RequestStageService.java
@@ -1,5 +1,49 @@
 package com.springcourse.service;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcourse.domain.RequestStage;
+import com.springcourse.enums.RequestState;
+import com.springcourse.repository.RequestRepository;
+import com.springcourse.repository.RequestStageRepository;
+
+@Service
 public class RequestStageService {
+	
+	@Autowired private RequestStageRepository requestStageRepository;
+	@Autowired private RequestRepository requestRepository;
+	
+	//save
+	public RequestStage save(RequestStage stage) {
+		stage.setRealizationDate(new Date());
+		
+		RequestStage createdStage = requestStageRepository.save(stage);
+		
+		Long requestId = stage.getRequest().getId();
+		RequestState state = stage.getState();
+		
+		requestRepository.updateStatus(requestId, state);
+		
+		return createdStage;
+	}
+	
+	//get
+	public RequestStage getById(Long id) {
+		requestStageRepository.findById(id);
+		
+		Optional<RequestStage> result = requestStageRepository.findById(id);
+		return result.get();
+	}
+	
+	//get by id
+	public List<RequestStage> listAllByRequestId(Long requestId){
+		List<RequestStage> stages = requestStageRepository.findAllByRequestId(requestId);
+		return stages;
+	}
 
 }
