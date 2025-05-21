b8744081e5ad30514c06fff3801307d98f86aa69
rndsilva <rafaelnunesatrix@gmail.com>
2025-05-21 17:48:03 -0300

    create user login dto class

---
+++ src/main/java/com/springcourse/dto/UserLogindto.java
@@ -0,0 +1,32 @@
+package com.springcourse.dto;
+
+
+public class UserLogindto {
+	private String email;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public UserLogindto() {
+		
+	}
+	
+	public UserLogindto(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+
+}
