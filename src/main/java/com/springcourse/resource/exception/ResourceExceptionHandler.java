07d340e28112b80eb11b41db9ae1751efee3d2b3
rndsilva <rafaelnunesatrix@gmail.com>
2025-06-10 22:17:03 -0300

    Overrride argument not valid method

---
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.springcourse.resource.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.springcourse.exception.NotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<ApiError> handleNorFoundException(NotFoundException ex ) {
+		
+		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date()); 
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		// TODO Auto-generated method stub
+		
+		String defaultMessage= ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+		
+		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(),defaultMessage, new Date());
+		
+		//return super.handleMethodArgumentNotValid(ex, headers, status, request);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}  
+	
+
+	/*ADD Chat CPG*/
+/*	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+	    StringBuilder sb = new StringBuilder();
+
+	    ex.getBindingResult().getFieldErrors().forEach(error -> {
+	        sb.append(error.getField())
+	          .append(": ")
+	          .append(error.getDefaultMessage())
+	          .append("; ");
+	    });
+
+	    ApiError error = new ApiError(
+	        HttpStatus.BAD_REQUEST.value(),
+	        sb.toString(),
+	        new Date()
+	    );
+
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	*/
+	/*ADD Chat CPG*/
+}
